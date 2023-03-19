85436e24dc7e9e7c669651abd900f01dc38daa5c
lerapotato <potato.lera1994@gmail.com>
2023-03-20 01:54:38 +0800

    initial commit

---
+++ src/SearchItem.js
@@ -0,0 +1,20 @@
+
+
+const SearchItem = ({search, setSearch}) => {
+  return (
+    <form className="searchForm" onSubmit={(e)=> e.preventDefault()}>
+        <label 
+        htmlFor="search"
+        >Search</label>
+        <input 
+        type="text"
+        id="search"
+        role="searchbox"
+        placeholder="Search Item"
+        value={search}
+        onChange={(e)=> setSearch(e.target.value)} />
+    </form>
+  )
+}
+
+export default SearchItem
